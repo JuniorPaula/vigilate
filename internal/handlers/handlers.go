d52cf5c2fe24cd32491cc155ceb1b9fb0efca3e0
Junior Paula <luke.junnior@icloud.com>
2023-09-01 08:28:44 -0300

    feat: add form validation, and setting up a handler for posting data

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -134,6 +134,12 @@ func (repo *DBRepo) Host(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// PostHost adds/edits a host
+func (repo *DBRepo) PostHost(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte("Post Host"))
+}
+
 // AllUsers lists all admin users
 func (repo *DBRepo) AllUsers(w http.ResponseWriter, r *http.Request) {
 	vars := make(jet.VarMap)
